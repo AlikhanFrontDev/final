db3409c33c54771289ed56d250e95166b460110d
AlikhanFrontDev <alixanhellion@gmail.comgit config --global user.email alixanhellion@gmail.com>
2022-11-04 08:41:05 +0500

    routers

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -29,13 +29,13 @@ const handleLogin = async (e) => {
                 const userrole = roleName.roles
 
                 if (userrole === "SUPER_ADMIN") {
-                    supernavigate('/final/dashboard')
+                    supernavigate('/dashboard')
                 }
                 else if (userrole === "ADMIN") {
-                    adminnavigate('/final/schedule')
+                    adminnavigate('/schedule')
                 }
                 else if (userrole === "CASHIER") {
-                    cashiernavigate('/final/debtors')
+                    cashiernavigate('/debtors')
                 }
                 else {
                     noUserNavigate("/")
